react-navigation/drawer

refactor: rewrite drawer layout with reanimated

src/views/DrawerView.tsx
@@ -1,30 +1,28 @@
 import * as React from 'react';
-import { Dimensions, StyleSheet, ViewStyle, Animated } from 'react-native';
+import { Dimensions, StyleSheet, ViewStyle } from 'react-native';
 import { SceneView } from '@react-navigation/core';
-import DrawerLayout from 'react-native-gesture-handler/DrawerLayout';
 import { ScreenContainer } from 'react-native-screens';
 
 import * as DrawerActions from '../routers/DrawerActions';
 import DrawerSidebar, { ContentComponentProps } from './DrawerSidebar';
 import DrawerGestureContext from '../utils/DrawerGestureContext';
-import ResourceSavingScene from '../views/ResourceSavingScene';
+import ResourceSavingScene from './ResourceSavingScene';
+import Drawer from './Drawer';
 import { Navigation } from '../types';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 
 type DrawerOptions = {
   drawerBackgroundColor?: string;
   overlayColor?: string;
   minSwipeDistance?: number;
   drawerPosition: 'left' | 'right';
+  drawerType: 'front' | 'back' | 'slide';
   drawerLockMode?: 'unlocked' | 'locked-closed' | 'locked-open';
   keyboardDismissMode?: 'on-drag' | 'none';
-  drawerType: 'front' | 'back' | 'slide';
   drawerWidth: number | (() => number);
   statusBarAnimation: 'slide' | 'none' | 'fade';
-  useNativeAnimations?: boolean;
   onDrawerClose?: () => void;
   onDrawerOpen?: () => void;
-  onDrawerStateChanged?: () => void;
-  drawerContainerStyle?: ViewStyle;
   contentContainerStyle?: ViewStyle;
   edgeWidth: number;
   hideStatusBar?: boolean;
@@ -82,90 +80,30 @@ export default class DrawerView extends React.PureComponent<Props, State> {
   };
 
   componentDidMount() {
-    Dimensions.addEventListener('change', this._updateWidth);
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    const {
-      openId,
-      closeId,
-      toggleId,
-      isDrawerOpen,
-    } = this.props.navigation.state;
-    const {
-      openId: prevOpenId,
-      closeId: prevCloseId,
-      toggleId: prevToggleId,
-    } = prevProps.navigation.state;
-
-    let prevIds = [prevOpenId, prevCloseId, prevToggleId];
-    let changedIds = [openId, closeId, toggleId]
-      .filter(id => !prevIds.includes(id))
-      // @ts-ignore
-      .sort((a, b) => a > b);
-
-    changedIds.forEach(id => {
-      if (id === openId) {
-        this._drawer.openDrawer();
-      } else if (id === closeId) {
-        this._drawer.closeDrawer();
-      } else if (id === toggleId) {
-        if (isDrawerOpen) {
-          this._drawer.closeDrawer();
-        } else {
-          this._drawer.openDrawer();
-        }
-      }
-    });
+    Dimensions.addEventListener('change', this.updateWidth);
   }
 
   componentWillUnmount() {
-    Dimensions.removeEventListener('change', this._updateWidth);
+    Dimensions.removeEventListener('change', this.updateWidth);
   }
 
-  _drawer: typeof DrawerLayout;
+  private drawerGestureRef = React.createRef<PanGestureHandler>();
 
-  drawerGestureRef = React.createRef();
-
-  _handleDrawerStateChange = (newState: string, willShow: boolean) => {
-    if (newState === 'Idle') {
-      if (!this.props.navigation.state.isDrawerIdle) {
-        this.props.navigation.dispatch({
-          type: DrawerActions.MARK_DRAWER_IDLE,
-          key: this.props.navigation.state.key,
-        });
-      }
-    } else if (newState === 'Settling') {
-      this.props.navigation.dispatch({
-        type: DrawerActions.MARK_DRAWER_SETTLING,
-        key: this.props.navigation.state.key,
-        willShow,
-      });
-    } else {
-      if (this.props.navigation.state.isDrawerIdle) {
-        this.props.navigation.dispatch({
-          type: DrawerActions.MARK_DRAWER_ACTIVE,
-          key: this.props.navigation.state.key,
-        });
-      }
-    }
-  };
-
-  _handleDrawerOpen = () => {
+  private handleDrawerOpen = () => {
     this.props.navigation.dispatch({
-      type: DrawerActions.DRAWER_OPENED,
+      type: DrawerActions.OPEN_DRAWER,
       key: this.props.navigation.state.key,
     });
   };
 
-  _handleDrawerClose = () => {
+  private handleDrawerClose = () => {
     this.props.navigation.dispatch({
-      type: DrawerActions.DRAWER_CLOSED,
+      type: DrawerActions.CLOSE_DRAWER,
       key: this.props.navigation.state.key,
     });
   };
 
-  _updateWidth = () => {
+  private updateWidth = () => {
     const drawerWidth =
       typeof this.props.navigationConfig.drawerWidth === 'function'
         ? this.props.navigationConfig.drawerWidth()
@@ -176,27 +114,23 @@ export default class DrawerView extends React.PureComponent<Props, State> {
     }
   };
 
-  _renderNavigationView = (
-    drawerOpenProgress: Animated.AnimatedInterpolation
-  ) => {
+  private renderNavigationView = ({ progress }: any) => {
     return (
-      <DrawerGestureContext.Provider value={this.drawerGestureRef}>
-        <DrawerSidebar
-          screenProps={this.props.screenProps}
-          drawerOpenProgress={drawerOpenProgress}
-          navigation={this.props.navigation}
-          descriptors={this.props.descriptors}
-          contentComponent={this.props.navigationConfig.contentComponent}
-          contentOptions={this.props.navigationConfig.contentOptions}
-          drawerPosition={this.props.navigationConfig.drawerPosition}
-          style={this.props.navigationConfig.style}
-          {...this.props.navigationConfig}
-        />
-      </DrawerGestureContext.Provider>
+      <DrawerSidebar
+        screenProps={this.props.screenProps}
+        drawerOpenProgress={progress}
+        navigation={this.props.navigation}
+        descriptors={this.props.descriptors}
+        contentComponent={this.props.navigationConfig.contentComponent}
+        contentOptions={this.props.navigationConfig.contentOptions}
+        drawerPosition={this.props.navigationConfig.drawerPosition}
+        style={this.props.navigationConfig.style}
+        {...this.props.navigationConfig}
+      />
     );
   };
 
-  _renderContent = () => {
+  private renderContent = () => {
     let { lazy, navigation } = this.props;
     let { loaded } = this.state;
     let { routes } = navigation.state;
@@ -214,7 +148,7 @@ export default class DrawerView extends React.PureComponent<Props, State> {
       );
     } else {
       return (
-        <ScreenContainer style={styles.pages}>
+        <ScreenContainer style={styles.content}>
           {routes.map((route, index) => {
             if (lazy && !loaded.includes(index)) {
               // Don't render a screen if we've never navigated to it
@@ -246,67 +180,68 @@ export default class DrawerView extends React.PureComponent<Props, State> {
     }
   };
 
-  _setDrawerGestureRef = (ref: any) => {
+  private setDrawerGestureRef = (ref: PanGestureHandler | null) => {
     // @ts-ignore
     this.drawerGestureRef.current = ref;
   };
 
   render() {
-    const { navigation, screenProps } = this.props;
+    const { navigation } = this.props;
+    const {
+      drawerType,
+      drawerBackgroundColor,
+      overlayColor,
+      contentContainerStyle,
+      edgeWidth,
+      minSwipeDistance,
+      hideStatusBar,
+      statusBarAnimation,
+    } = this.props.navigationConfig;
     const activeKey = navigation.state.routes[navigation.state.index].key;
     const { drawerLockMode } = this.props.descriptors[activeKey].options;
 
+    const isOpen =
+      drawerLockMode === 'locked-closed'
+        ? false
+        : drawerLockMode === 'locked-open'
+        ? true
+        : this.props.navigation.state.isDrawerOpen;
+
     return (
-      <DrawerLayout
-        ref={(c: any) => {
-          this._drawer = c;
-        }}
-        onGestureRef={this._setDrawerGestureRef}
-        drawerLockMode={
-          drawerLockMode ||
-          (typeof screenProps === 'object' &&
-            screenProps != null &&
-            // @ts-ignore
-            screenProps.drawerLockMode) ||
-          this.props.navigationConfig.drawerLockMode
-        }
-        drawerBackgroundColor={
-          this.props.navigationConfig.drawerBackgroundColor
-        }
-        keyboardDismissMode={this.props.navigationConfig.keyboardDismissMode}
-        drawerWidth={this.state.drawerWidth}
-        onDrawerOpen={this._handleDrawerOpen}
-        onDrawerClose={this._handleDrawerClose}
-        onDrawerStateChanged={this._handleDrawerStateChange}
-        useNativeAnimations={this.props.navigationConfig.useNativeAnimations}
-        renderNavigationView={this._renderNavigationView}
-        drawerPosition={
-          this.props.navigationConfig.drawerPosition === 'right'
-            ? DrawerLayout.positions.Right
-            : DrawerLayout.positions.Left
-        }
-        /* props specific to react-native-gesture-handler/DrawerLayout */
-        drawerType={this.props.navigationConfig.drawerType}
-        edgeWidth={this.props.navigationConfig.edgeWidth}
-        hideStatusBar={this.props.navigationConfig.hideStatusBar}
-        statusBarAnimation={this.props.navigationConfig.statusBarAnimation}
-        minSwipeDistance={this.props.navigationConfig.minSwipeDistance}
-        overlayColor={this.props.navigationConfig.overlayColor}
-        drawerContainerStyle={this.props.navigationConfig.drawerContainerStyle}
-        contentContainerStyle={
-          this.props.navigationConfig.contentContainerStyle
-        }
-      >
-        <DrawerGestureContext.Provider value={this.drawerGestureRef}>
-          {this._renderContent()}
-        </DrawerGestureContext.Provider>
-      </DrawerLayout>
+      <DrawerGestureContext.Provider value={this.drawerGestureRef}>
+        <Drawer
+          open={isOpen}
+          locked={
+            drawerLockMode === 'locked-open' ||
+            drawerLockMode === 'locked-closed'
+          }
+          onOpen={this.handleDrawerOpen}
+          onClose={this.handleDrawerClose}
+          onGestureRef={this.setDrawerGestureRef}
+          drawerType={drawerType}
+          drawerPosition={this.props.navigationConfig.drawerPosition}
+          contentContainerStyle={contentContainerStyle}
+          drawerStyle={{
+            backgroundColor: drawerBackgroundColor || 'white',
+            width: this.state.drawerWidth,
+          }}
+          overlayStyle={{
+            backgroundColor: overlayColor || 'rgba(0, 0, 0, 0.5)',
+          }}
+          swipeEdgeWidth={edgeWidth}
+          swipeDistanceThreshold={minSwipeDistance}
+          hideStatusBar={hideStatusBar}
+          statusBarAnimation={statusBarAnimation}
+          renderDrawerContent={this.renderNavigationView}
+          renderMainContent={this.renderContent}
+        />
+      </DrawerGestureContext.Provider>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  pages: {
+  content: {
     flex: 1,
   },
 });

grifotv/react-navigation-drawer

src/views/DrawerView.tsx
@@ -1,30 +1,28 @@
 import * as React from 'react';
-import { Dimensions, StyleSheet, ViewStyle, Animated } from 'react-native';
+import { Dimensions, StyleSheet, ViewStyle } from 'react-native';
 import { SceneView } from '@react-navigation/core';
-import DrawerLayout from 'react-native-gesture-handler/DrawerLayout';
 import { ScreenContainer } from 'react-native-screens';
 
 import * as DrawerActions from '../routers/DrawerActions';
 import DrawerSidebar, { ContentComponentProps } from './DrawerSidebar';
 import DrawerGestureContext from '../utils/DrawerGestureContext';
-import ResourceSavingScene from '../views/ResourceSavingScene';
+import ResourceSavingScene from './ResourceSavingScene';
+import Drawer from './Drawer';
 import { Navigation } from '../types';
+import { PanGestureHandler } from 'react-native-gesture-handler';
 
 type DrawerOptions = {
   drawerBackgroundColor?: string;
   overlayColor?: string;
   minSwipeDistance?: number;
   drawerPosition: 'left' | 'right';
+  drawerType: 'front' | 'back' | 'slide';
   drawerLockMode?: 'unlocked' | 'locked-closed' | 'locked-open';
   keyboardDismissMode?: 'on-drag' | 'none';
-  drawerType: 'front' | 'back' | 'slide';
   drawerWidth: number | (() => number);
   statusBarAnimation: 'slide' | 'none' | 'fade';
-  useNativeAnimations?: boolean;
   onDrawerClose?: () => void;
   onDrawerOpen?: () => void;
-  onDrawerStateChanged?: () => void;
-  drawerContainerStyle?: ViewStyle;
   contentContainerStyle?: ViewStyle;
   edgeWidth: number;
   hideStatusBar?: boolean;
@@ -82,90 +80,36 @@ export default class DrawerView extends React.PureComponent<Props, State> {
   };
 
   componentDidMount() {
-    Dimensions.addEventListener('change', this._updateWidth);
-  }
-
-  componentDidUpdate(prevProps: Props) {
-    const {
-      openId,
-      closeId,
-      toggleId,
-      isDrawerOpen,
-    } = this.props.navigation.state;
-    const {
-      openId: prevOpenId,
-      closeId: prevCloseId,
-      toggleId: prevToggleId,
-    } = prevProps.navigation.state;
-
-    let prevIds = [prevOpenId, prevCloseId, prevToggleId];
-    let changedIds = [openId, closeId, toggleId]
-      .filter(id => !prevIds.includes(id))
-      // @ts-ignore
-      .sort((a, b) => a > b);
-
-    changedIds.forEach(id => {
-      if (id === openId) {
-        this._drawer.openDrawer();
-      } else if (id === closeId) {
-        this._drawer.closeDrawer();
-      } else if (id === toggleId) {
-        if (isDrawerOpen) {
-          this._drawer.closeDrawer();
-        } else {
-          this._drawer.openDrawer();
-        }
-      }
-    });
+    Dimensions.addEventListener('change', this.updateWidth);
   }
 
   componentWillUnmount() {
-    Dimensions.removeEventListener('change', this._updateWidth);
+    Dimensions.removeEventListener('change', this.updateWidth);
   }
 
-  _drawer: typeof DrawerLayout;
+  private drawerGestureRef = React.createRef<PanGestureHandler>();
 
-  drawerGestureRef = React.createRef();
+  private handleDrawerOpen = () => {
+    const { navigation } = this.props;
 
-  _handleDrawerStateChange = (newState: string, willShow: boolean) => {
-    if (newState === 'Idle') {
-      if (!this.props.navigation.state.isDrawerIdle) {
-        this.props.navigation.dispatch({
-          type: DrawerActions.MARK_DRAWER_IDLE,
-          key: this.props.navigation.state.key,
-        });
-      }
-    } else if (newState === 'Settling') {
-      this.props.navigation.dispatch({
-        type: DrawerActions.MARK_DRAWER_SETTLING,
-        key: this.props.navigation.state.key,
-        willShow,
-      });
-    } else {
-      if (this.props.navigation.state.isDrawerIdle) {
-        this.props.navigation.dispatch({
-          type: DrawerActions.MARK_DRAWER_ACTIVE,
-          key: this.props.navigation.state.key,
-        });
-      }
-    }
+    navigation.dispatch(
+      DrawerActions.openDrawer({
+        key: navigation.state.key,
+      })
+    );
   };
 
-  _handleDrawerOpen = () => {
-    this.props.navigation.dispatch({
-      type: DrawerActions.DRAWER_OPENED,
-      key: this.props.navigation.state.key,
-    });
+  private handleDrawerClose = () => {
+    const { navigation } = this.props;
+
+    navigation.dispatch(
+      DrawerActions.closeDrawer({
+        key: navigation.state.key,
+      })
+    );
   };
 
-  _handleDrawerClose = () => {
-    this.props.navigation.dispatch({
-      type: DrawerActions.DRAWER_CLOSED,
-      key: this.props.navigation.state.key,
-    });
-  };
-
-  _updateWidth = () => {
+  private updateWidth = () => {
     const drawerWidth =
       typeof this.props.navigationConfig.drawerWidth === 'function'
         ? this.props.navigationConfig.drawerWidth()
@@ -176,27 +120,23 @@ export default class DrawerView extends React.PureComponent<Props, State> {
     }
   };
 
-  _renderNavigationView = (
-    drawerOpenProgress: Animated.AnimatedInterpolation
-  ) => {
+  private renderNavigationView = ({ progress }: any) => {
     return (
-      <DrawerGestureContext.Provider value={this.drawerGestureRef}>
-        <DrawerSidebar
-          screenProps={this.props.screenProps}
-          drawerOpenProgress={drawerOpenProgress}
-          navigation={this.props.navigation}
-          descriptors={this.props.descriptors}
-          contentComponent={this.props.navigationConfig.contentComponent}
-          contentOptions={this.props.navigationConfig.contentOptions}
-          drawerPosition={this.props.navigationConfig.drawerPosition}
-          style={this.props.navigationConfig.style}
-          {...this.props.navigationConfig}
-        />
-      </DrawerGestureContext.Provider>
+      <DrawerSidebar
+        screenProps={this.props.screenProps}
+        drawerOpenProgress={progress}
+        navigation={this.props.navigation}
+        descriptors={this.props.descriptors}
+        contentComponent={this.props.navigationConfig.contentComponent}
+        contentOptions={this.props.navigationConfig.contentOptions}
+        drawerPosition={this.props.navigationConfig.drawerPosition}
+        style={this.props.navigationConfig.style}
+        {...this.props.navigationConfig}
+      />
     );
   };
 
-  _renderContent = () => {
+  private renderContent = () => {
     let { lazy, navigation } = this.props;
     let { loaded } = this.state;
     let { routes } = navigation.state;
@@ -214,7 +154,7 @@ export default class DrawerView extends React.PureComponent<Props, State> {
       );
     } else {
       return (
-        <ScreenContainer style={styles.pages}>
+        <ScreenContainer style={styles.content}>
           {routes.map((route, index) => {
             if (lazy && !loaded.includes(index)) {
               // Don't render a screen if we've never navigated to it
@@ -246,67 +186,68 @@ export default class DrawerView extends React.PureComponent<Props, State> {
     }
   };
 
-  _setDrawerGestureRef = (ref: any) => {
+  private setDrawerGestureRef = (ref: PanGestureHandler | null) => {
     // @ts-ignore
     this.drawerGestureRef.current = ref;
   };
 
   render() {
-    const { navigation, screenProps } = this.props;
+    const { navigation } = this.props;
+    const {
+      drawerType,
+      drawerBackgroundColor,
+      overlayColor,
+      contentContainerStyle,
+      edgeWidth,
+      minSwipeDistance,
+      hideStatusBar,
+      statusBarAnimation,
+    } = this.props.navigationConfig;
     const activeKey = navigation.state.routes[navigation.state.index].key;
     const { drawerLockMode } = this.props.descriptors[activeKey].options;
 
+    const isOpen =
+      drawerLockMode === 'locked-closed'
+        ? false
+        : drawerLockMode === 'locked-open'
+        ? true
+        : this.props.navigation.state.isDrawerOpen;
+
     return (
-      <DrawerLayout
-        ref={(c: any) => {
-          this._drawer = c;
-        }}
-        onGestureRef={this._setDrawerGestureRef}
-        drawerLockMode={
-          drawerLockMode ||
-          (typeof screenProps === 'object' &&
-            screenProps != null &&
-            // @ts-ignore
-            screenProps.drawerLockMode) ||
-          this.props.navigationConfig.drawerLockMode
-        }
-        drawerBackgroundColor={
-          this.props.navigationConfig.drawerBackgroundColor
-        }
-        keyboardDismissMode={this.props.navigationConfig.keyboardDismissMode}
-        drawerWidth={this.state.drawerWidth}
-        onDrawerOpen={this._handleDrawerOpen}
-        onDrawerClose={this._handleDrawerClose}
-        onDrawerStateChanged={this._handleDrawerStateChange}
-        useNativeAnimations={this.props.navigationConfig.useNativeAnimations}
-        renderNavigationView={this._renderNavigationView}
-        drawerPosition={
-          this.props.navigationConfig.drawerPosition === 'right'
-            ? DrawerLayout.positions.Right
-            : DrawerLayout.positions.Left
-        }
-        /* props specific to react-native-gesture-handler/DrawerLayout */
-        drawerType={this.props.navigationConfig.drawerType}
-        edgeWidth={this.props.navigationConfig.edgeWidth}
-        hideStatusBar={this.props.navigationConfig.hideStatusBar}
-        statusBarAnimation={this.props.navigationConfig.statusBarAnimation}
-        minSwipeDistance={this.props.navigationConfig.minSwipeDistance}
-        overlayColor={this.props.navigationConfig.overlayColor}
-        drawerContainerStyle={this.props.navigationConfig.drawerContainerStyle}
-        contentContainerStyle={
-          this.props.navigationConfig.contentContainerStyle
-        }
-      >
-        <DrawerGestureContext.Provider value={this.drawerGestureRef}>
-          {this._renderContent()}
-        </DrawerGestureContext.Provider>
-      </DrawerLayout>
+      <DrawerGestureContext.Provider value={this.drawerGestureRef}>
+        <Drawer
+          open={isOpen}
+          locked={
+            drawerLockMode === 'locked-open' ||
+            drawerLockMode === 'locked-closed'
+          }
+          onOpen={this.handleDrawerOpen}
+          onClose={this.handleDrawerClose}
+          onGestureRef={this.setDrawerGestureRef}
+          drawerType={drawerType}
+          drawerPosition={this.props.navigationConfig.drawerPosition}
+          contentContainerStyle={contentContainerStyle}
+          drawerStyle={{
+            backgroundColor: drawerBackgroundColor || 'white',
+            width: this.state.drawerWidth,
+          }}
+          overlayStyle={{
+            backgroundColor: overlayColor || 'rgba(0, 0, 0, 0.5)',
+          }}
+          swipeEdgeWidth={edgeWidth}
+          swipeDistanceThreshold={minSwipeDistance}
+          hideStatusBar={hideStatusBar}
+          statusBarAnimation={statusBarAnimation}
+          renderDrawerContent={this.renderNavigationView}
+          renderSceneContent={this.renderContent}
+        />
+      </DrawerGestureContext.Provider>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  pages: {
+  content: {
     flex: 1,
   },
 });